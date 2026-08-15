b55613b3c1b9f8bbbac275eeff483ef9b766ad2a
lian <106605983+Liah051@users.noreply.github.com>
2026-08-15 12:27:52 +0900

    微修正

--- astro.config.ts
+++ astro.config.ts
@@ -21,22 +21,29 @@ import { transformerFileName } from "./src/utils/transformers/fileName";
 import { SITE } from "./src/config";
 
 import react from "@astrojs/react";
+import iosBackNavFix from "@tinloof/astro-ios-backnav-fix";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [sitemap({
-    filter: page => SITE.showArchives || !page.endsWith("/archives"),
-  }), mdx(), d2({
-    theme: {
-      default: '0',    // 明るいテーマ
-      dark: '200',     // ダークモード用テーマ
-    },
-    inline: true,      // SVGをHTMLに直接埋め込む
-    experimental: {
-      useD2js: true,    // WASM版のD2を使用（アダプティブ・ダークモードに対応）
-    },
-  }), react()],
+  integrations: [
+    iosBackNavFix(),
+    sitemap({
+      filter: page => SITE.showArchives || !page.endsWith("/archives"),
+    }),
+    mdx(),
+    d2({
+      theme: {
+        default: '0',    // 明るいテーマ
+        dark: '200',     // ダークモード用テーマ
+      },
+      inline: true,      // SVGをHTMLに直接埋め込む
+      experimental: {
+        useD2js: true,    // WASM版のD2を使用（アダプティブ・ダークモードに対応）
+      },
+    }),
+    react()
+  ],
   markdown: {
     remarkPlugins: [
       remarkStrongJsx,
